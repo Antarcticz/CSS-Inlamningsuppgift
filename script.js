ef5318af2eaa232d7e0fb4de0c066cf8387454c2
Maximilian <maximilian_hedman@hotmail.com>
2022-11-20 02:14:07 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/*  Använt absolut inte eftersom om du skapar en funktion eller ett 
+    värde med var och senare längre ner använder var och med samma variabel namn, 
+    så kommer variabel namnet längre ner i koden skriva över den första, 
+    och där med kommer inte din första funktion eller värde fungera */
+// var test1 = 0;
+
+// //  Använt om variabeln får ändras
+// let test2 = 0;
+
+// //  Använd om variabeln inte får ändras
+// const test3 = 0;
+
+
+// /*  Använd inte eftersom när den här funktionen körs så kommer 
+//     den slängas upp längst upp i koden även om det inte syns här*/
+// function test () {
+
+// }
+
+// //  Går mycket bra att använda denna
+// const enFunktion = funktion () {
+
+// }
+
+// //  Nyaste sättet att skapa en funktion där function är =>
+// const enTillFunktion = () => {
+
+// }
+
+//      ser du mig?
+//*     ser du mig?
+//todo  ser du mig?
+//!     ser du mig?
+
